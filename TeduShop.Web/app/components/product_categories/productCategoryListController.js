1ded558b9072582c76906c14134613323d4fd900
DESKTOP-1E2DK9Q\Admin <tranthanhtrung.fithou@gmail.com>
2021-09-09 03:26:13 +0700

    bai 27

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,9 +1,10 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService
+    ) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pageCount = 0;
@@ -26,6 +27,13 @@
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                //if (result.data.totalCount == 0) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không có bản ghi nào được tìm thấy');
+                }
+                else {
+                    notificationService.displaySuccess('Tìm thấy ' + result.data.TotalCount + ' bản ghi');
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
